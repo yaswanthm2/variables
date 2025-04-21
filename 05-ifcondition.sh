80c85dc4c6250e7d8dd459c1db8321c3938b82dd
Yaswanth <yaswanth@>
2025-04-21 11:25:49 +0530

    if condition  script

--- 05-ifcondition.sh
+++ 05-ifcondition.sh
@@ -2,10 +2,10 @@
 
 USERID=$(id -u)
 
-if [$USERID==0]
+if [ $USERID -nt 0 ]
     then
         DATE=$(date)
         echo " date is : $DATE"
     else 
-       echo "please change the user to root"
+       echo "please change the user to normal"
 fi    
