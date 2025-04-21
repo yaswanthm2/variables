68e5970852b38344df81cb0a1ef882568354d378
Yaswanth <yaswanth@>
2025-04-21 22:06:15 +0530

    assignment script

---
+++ 06-assignment.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+R=\e[31m    #it will give text red color 
+G=\e[32m    #it will give text green color 
+N=\e[0m     #it will give text normal color 
+
+SCRIPTNAME=$0     # $0 give script file name to that variable  
+DATE=$(date +%F-%H:%M:%S)   
+LOGFILE=/tmp/$SCRIPTNAME-$DATE.log    # this is the path where log files are stored
+USER=$(id -u)
+
+if [ $USER -ne 0 ]
+  then
+    echo -e "please change user to $R sudo user"
+    exit 1
+  else
+    echo -e "$G user is root user"
+
+validate(){
+    if [ $1 -ne o ]
+    then
+       echo  -e "$R $2 ...  failure"
+       exit 1
+    else
+       echo -e "$G $2 ... sucess"
+    fi            
+}
+
+for i in $@   # $@ it refers all the arguments you are giving while executing script 
+   do 
+     yum install $i -y >>LOGFILE 
+     validate $?  "Installing $i"     # $? it is exit status code means if command is succes it give 0 value if not it will give 1-127
+   done
+
+
+
+
